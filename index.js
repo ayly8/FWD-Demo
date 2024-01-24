b5b6b068bfc607b71b55390206e6e9cca4d8371a
ayly8 <allison.y.ly@gmail.com>
2024-01-23 23:17:06 -0800

    edited html, css, and added js file to handle button actions

---
+++ index.js
@@ -0,0 +1,8 @@
+function display_ans() {
+   const ans = ["It is certain", "Without a doubt", "Yes definitely",
+               "Reply hazy, try again", "Ask again later", "Cannot predit",
+               "Don't count on it", "My reply is no", "Very doubtful"];
+
+   const rand_ans = ans[(Math.floor(Math.random() * ans.length))];
+   document.getElementById("answer").innerHTML = "Answer: " + rand_ans;
+}
